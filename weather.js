464a519c184161c68a0aa3a55a773b738dca1ca6
Роман Кислов <roman@kislovs.ru>
2022-12-05 12:08:02 +0500

    first

---
+++ weather.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import { getArgs } from './helpers/args.js'
+import { printHelp } from './services/log.service.js';
+import { saveKeyValue } from './services/storage.service.js';
+
+const initCLI = () => {
+    const args = getArgs(process.argv);
+    if (args.h) {
+        printHelp();
+    };
+    if (args.s){
+
+    };
+    if (args.t){
+        saveKeyValue('token', args.t);
+    };
+    
+};
+
+initCLI();
